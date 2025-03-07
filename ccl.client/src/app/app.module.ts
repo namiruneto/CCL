8c0b89f599124cb1cdb4514b007c1c978c8b842a
namiruneto <cesfernando1212@gmail.com>
2025-03-07 01:45:04 -0500

    feat: soporte para formularios y gestión de cookies
    Se ha importado `CookieService` para gestionar cookies.
    Se han añadido los componentes `LoginComponent`, `ProductosComponent` y `ProductoFormComponent` al módulo.
    Se ha incluido `FormsModule` para habilitar formularios en la aplicación.
    Se han actualizado las secciones de `declarations`, `imports` y `providers` en `app.module.ts`.

--- ccl.client/src/app/app.module.ts
+++ ccl.client/src/app/app.module.ts
@@ -1,19 +1,29 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CookieService } from 'ngx-cookie-service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { ProductosComponent } from './Pages/productos/productos.component';
+import { ProductoFormComponent } from './Pages/producto-form/producto-form.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+          ProductosComponent,
+          ProductoFormComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule, FormsModule 
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
