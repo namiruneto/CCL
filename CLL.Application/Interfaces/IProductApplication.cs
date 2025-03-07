72e4dac41c8bd76a7b45f02186187b111b721438
namiruneto <cesfernando1212@gmail.com>
2025-03-06 22:44:30 -0500

    feat: interfaz IProductApplication
    Se ha creado la interfaz `IProductApplication` en `IProductApplication.cs`, que incluye los métodos `Inventory` y `Movimiento`. Se han añadido comentarios XML para documentar la interfaz y sus métodos.

---
+++ CLL.Application/Interfaces/IProductApplication.cs
@@ -0,0 +1,24 @@
+﻿namespace CLL.Application.Interfaces
+{
+    using CLL.Entities.Dto.Repository;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Defines the <see cref="IProductApplication" />.
+    /// </summary>
+    public interface IProductApplication
+    {
+        /// <summary>
+        /// The Inventory.
+        /// </summary>
+        /// <returns>The <see cref="List{ProductRepositoryDto}"/>.</returns>
+        List<ProductRepositoryDto> Inventory();
+
+        /// <summary>
+        /// The Movimiento.
+        /// </summary>
+        /// <param name="product">The product<see cref="ProductRepositoryDto"/>.</param>
+        /// <returns>The <see cref="bool"/>.</returns>
+        bool Movimiento(ProductRepositoryDto product);
+    }
+}
