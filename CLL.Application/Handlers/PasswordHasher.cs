aab2e053926de1e08f1e8236a3ac844a12fc4e31
namiruneto <cesfernando1212@gmail.com>
2025-03-06 21:59:09 -0500

    feat: encritar clave para velidar en base de datos

---
+++ CLL.Application/Handlers/PasswordHasher.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="PasswordHasher.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CLL.Application.Handlers
+{
+    using System.Security.Cryptography;
+    using System.Text;
+
+    /// <summary>
+    /// Defines the <see cref="PasswordHasher" />.
+    /// </summary>
+    public class PasswordHasher
+    {
+        /// <summary>
+        /// The EncryptPassword.
+        /// </summary>
+        /// <param name="password">The password<see cref="string"/>.</param>
+        /// <returns>The <see cref="string"/>.</returns>
+        public string EncryptPassword(string password)
+        {
+            // Convertir la contraseña a un array de bytes
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+            // Crear una instancia del algoritmo de hash SHA256
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                // Obtener el hash de la contraseña
+                byte[] hashBytes = sha256.ComputeHash(passwordBytes);
+
+                // Convertir el hash a una cadena Base64
+                return Convert.ToBase64String(hashBytes);
+            }
+        }
+    }
+}
