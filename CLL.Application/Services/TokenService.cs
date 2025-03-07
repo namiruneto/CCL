47cabea203a4a923c9a042c6e058979400f99d11
namiruneto <cesfernando1212@gmail.com>
2025-03-06 22:00:20 -0500

    feat:  Se ha implementado la clase interna `TokenService` en `TokenService.cs`, que incluye los métodos `GenerateToken` para generar tokens JWT y `ValidateToken` para validar tokens. Se utilizan claves secretas almacenadas en las variables de entorno y se han importado los espacios de nombres necesarios para su funcionamiento.

---
+++ CLL.Application/Services/TokenService.cs
@@ -0,0 +1,63 @@
+﻿// <copyright file="TokenService.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CLL.Application.Services
+{
+    using Microsoft.IdentityModel.Tokens;
+    using System;
+    using System.IdentityModel.Tokens.Jwt;
+    using System.Security.Claims;
+
+    /// <summary>
+    /// Defines the <see cref="TokenService" />.
+    /// </summary>
+    internal class TokenService
+    {
+        /// <summary>
+        /// The GenerateToken.
+        /// </summary>
+        /// <param name="username">The username<see cref="string"/>.</param>
+        /// <returns>The <see cref="string"/>.</returns>
+        public string GenerateToken(string username)
+        {
+            string va = Environment.GetEnvironmentVariable("MySecretKey");
+            var symmetricKey = new SymmetricSecurityKey(Convert.FromBase64String(Environment.GetEnvironmentVariable("MySecretKey")));
+            var credentials = new SigningCredentials(symmetricKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, username)
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: "BackEnd",
+                audience: "FrontEnd",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(11),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        /// <summary>
+        /// The ValidateToken.
+        /// </summary>
+        /// <param name="token">The token<see cref="string"/>.</param>
+        /// <returns>The <see cref="ClaimsPrincipal"/>.</returns>
+        public ClaimsPrincipal ValidateToken(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var validationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Convert.FromBase64String(Environment.GetEnvironmentVariable("MySecretKey"))),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+
+            return tokenHandler.ValidateToken(token, validationParameters, out var validatedToken);
+        }
+    }
+}
