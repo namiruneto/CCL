500d47bebb8912a296fafc14c766c340865735ad
namiruneto <cesfernando1212@gmail.com>
2025-03-06 22:44:07 -0500

    feat:  controlador ProductosController
    Se ha creado el controlador `ProductosController` en `CCL.Server.Controllers`, que incluye métodos para gestionar el inventario y el movimiento de productos. Este controlador utiliza `IProductApplication` y está configurado con atributos de ruta y autorización. Los métodos devuelven respuestas adecuadas basadas en la interacción con el servicio.

---
+++ CCL.Server/Controllers/ProductosController.cs
@@ -0,0 +1,61 @@
+﻿namespace CCL.Server.Controllers
+{
+    using CLL.Application.Interfaces;
+    using CLL.Entities.Dto.Request;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// Defines the <see cref="ProductosController" />.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ProductosController : Controller
+    {
+        /// <summary>
+        /// Defines the _services.
+        /// </summary>
+        private readonly IProductApplication _services;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProductosController"/> class.
+        /// </summary>
+        /// <param name="service">The service<see cref="IProductApplication"/>.</param>
+        public ProductosController(IProductApplication service)
+        {
+            _services = service;
+        }
+
+        /// <summary>
+        /// The inventario.
+        /// </summary>
+        /// <returns>The <see cref="Task{IActionResult}"/>.</returns>
+        [HttpGet("inventario")]
+        public async Task<IActionResult> inventario()
+        {
+            var result = _services.Inventory();
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            return Unauthorized();
+        }
+
+        /// <summary>
+        /// The movimiento.
+        /// </summary>
+        /// <param name="product">The product<see cref="ProductRequestDto"/>.</param>
+        /// <returns>The <see cref="Task{IActionResult}"/>.</returns>
+        [HttpPost("movimiento")]
+        public async Task<IActionResult> movimiento(ProductRequestDto product)
+        {
+            var result = _services.Movimiento(product);
+            if (result)
+            {
+                return Ok(result);
+            }
+            return Unauthorized();
+        }
+    }
+}
