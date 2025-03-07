2835de63dc87d0f83d491c5cec6daba24bda7144
namiruneto <cesfernando1212@gmail.com>
2025-03-06 22:01:12 -0500

    feat: Añadir interfaz ILoginDomain y su implementación
    Se ha creado la interfaz `ILoginDomain` con un método para validar la contraseña del usuario. La clase `LoginDomain` ha sido actualizada para implementar esta interfaz y proporcionar la lógica necesaria utilizando un repositorio de usuarios.

---
+++ CLL.Domain/Interfaces/ILoginDomain.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="ILoginDomain.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CLL.Domain.Interfaces
+{
+    using CLL.Entities.Dto.Request;
+
+    /// <summary>
+    /// Defines the <see cref="ILoginDomain" />.
+    /// </summary>
+    public interface ILoginDomain
+    {
+        /// <summary>
+        /// The ValidatePassword.
+        /// </summary>
+        /// <param name="loginRequestDto">The loginRequestDto<see cref="LoginRequestDto"/>.</param>
+        /// <returns>The <see cref="bool"/>.</returns>
+        bool ValidatePassword(LoginRequestDto loginRequestDto);
+    }
+}
