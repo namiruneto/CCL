4299a5ee14e7aeace71002dd51e133ea81181f38
namiruneto <cesfernando1212@gmail.com>
2025-03-06 22:47:02 -0500

    feat: copyright y definir ProductRequestDto
    Se ha añadido un comentario de copyright al inicio del archivo `ProductRequestDto.cs`.
    También se ha definido la clase `ProductRequestDto`, que hereda de `ProductRepositoryDto`,
    junto con un resumen en la documentación de la clase.

---
+++ CLL.Entities/Dto/Request/ProductRequestDto.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="ProductRequestDto.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CLL.Entities.Dto.Request
+{
+    using CLL.Entities.Dto.Repository;
+
+    /// <summary>
+    /// Defines the <see cref="ProductRequestDto" />.
+    /// </summary>
+    public class ProductRequestDto : ProductRepositoryDto
+    {
+    }
+}
