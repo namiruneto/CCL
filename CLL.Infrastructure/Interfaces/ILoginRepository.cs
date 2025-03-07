f8b91a65e642782e63a61911a6e154f9e9517cf0
namiruneto <cesfernando1212@gmail.com>
2025-03-06 22:04:35 -0500

    feat: Añadir interfaz y clase para gestión de usuarios
    Se ha creado la interfaz `ILoginRepository` con el método `ObtenerUsuario` para la obtención de usuarios mediante nombre de usuario y contraseña. Además, se ha implementado la clase `LoginRepository`, que utiliza Entity Framework para realizar la consulta y devolver un objeto `LoginRepositoryDto`.

---
+++ CLL.Infrastructure/Interfaces/ILoginRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace CLL.Infrastructure.Interfaces
+{
+    using CLL.Entities.Dto.Repository;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Defines the <see cref="ILoginRepository" />.
+    /// </summary>
+    public interface ILoginRepository
+    {
+        /// <summary>
+        /// The ObtenerUsuario.
+        /// </summary>
+        /// <param name="user">The user<see cref="string"/>.</param>
+        /// <param name="password">The password<see cref="string"/>.</param>
+        /// <returns>The <see cref="Task{LoginRepositoryDto}"/>.</returns>
+        Task<LoginRepositoryDto> ObtenerUsuario(string user, string password);
+    }
+}
---
+++ CLL.Infrastructure/Repository/LoginRepository.cs
@@ -0,0 +1,40 @@
+﻿namespace CLL.Infrastructure.Repository
+{
+    using CLL.Entities.Dto.Repository;
+    using CLL.Infrastructure.Data;
+    using CLL.Infrastructure.Interfaces;
+    using Microsoft.EntityFrameworkCore;
+
+    /// <summary>
+    /// Defines the <see cref="LoginRepository" />.
+    /// </summary>
+    public class LoginRepository : ILoginRepository
+    {
+        /// <summary>
+        /// Defines the _context.
+        /// </summary>
+        private readonly ApplicationDbContext _context;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoginRepository"/> class.
+        /// </summary>
+        /// <param name="context">The context<see cref="ApplicationDbContext"/>.</param>
+        public LoginRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <inheritdoc/>
+        public async Task<LoginRepositoryDto> ObtenerUsuario(string user, string password)
+        {
+            return await _context.usuarios
+               .Where(x => x.username == user && x.password == password)
+               .Select(x => new LoginRepositoryDto
+               {
+                   id = x.id,
+                   username = x.username,
+               })
+               .FirstOrDefaultAsync();
+        }
+    }
+}
