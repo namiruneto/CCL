cf8da62539dacbc302efd95ece9c4b456c30ea56
namiruneto <cesfernando1212@gmail.com>
2025-03-06 22:47:47 -0500

    feat: interfaz y clase de repositorio de productos
    Se ha creado la interfaz `IProductRepository` con métodos asíncronos para gestionar productos: `ObtenerProductos`, `Agregar` y `Actualizar`. Además, se ha implementado la clase `ProductRepository`, que utiliza Entity Framework para interactuar con la base de datos y proporciona las implementaciones de los métodos definidos en la interfaz.

---
+++ CLL.Infrastructure/Interfaces/IProductRepository.cs
@@ -0,0 +1,32 @@
+﻿namespace CLL.Infrastructure.Interfaces
+{
+    using CLL.Entities.Dto.Repository;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Defines the <see cref="IProductRepository" />.
+    /// </summary>
+    public interface IProductRepository
+    {
+        /// <summary>
+        /// The ObtenerProductos.
+        /// </summary>
+        /// <returns>The <see cref="Task{List{ProductRepositoryDto}}"/>.</returns>
+        Task<List<ProductRepositoryDto>> ObtenerProductos();
+
+        /// <summary>
+        /// The Agregar.
+        /// </summary>
+        /// <param name="cliente">The cliente<see cref="ProductRepositoryDto"/>.</param>
+        /// <returns>The <see cref="Task"/>.</returns>
+        Task Agregar(ProductRepositoryDto cliente);
+
+        /// <summary>
+        /// The Actualizar.
+        /// </summary>
+        /// <param name="cliente">The cliente<see cref="ProductRepositoryDto"/>.</param>
+        /// <returns>The <see cref="Task"/>.</returns>
+        Task Actualizar(ProductRepositoryDto cliente);
+    }
+}
