53578d05ab1d2a2334a6403cb89779fb6754e0ef
namiruneto <cesfernando1212@gmail.com>
2025-03-06 22:46:02 -0500

    feat: interfaz y clase de dominio de productos
    Se han añadido comentarios de copyright en `IProductDomain.cs` y `ProductDomain.cs`.
    Se ha definido la interfaz `IProductDomain` con métodos para gestionar el inventario y el movimiento de productos.
    La clase `ProductDomain` implementa esta interfaz y gestiona un repositorio de productos utilizando un parámetro de tipo `Lazy<IProductRepository>`.
    Los métodos `Inventory` y `Movimiento` permiten obtener productos y gestionar su adición o actualización según su ID.

---
+++ CLL.Domain/Interfaces/IProductDomain.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="IProductDomain.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CLL.Domain.Interfaces
+{
+    using CLL.Entities.Dto.Repository;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Defines the <see cref="IProductDomain" />.
+    /// </summary>
+    public interface IProductDomain
+    {
+        /// <summary>
+        /// The Inventory.
+        /// </summary>
+        /// <returns>The <see cref="List{ProductRepositoryDto}"/>.</returns>
+        List<ProductRepositoryDto> Inventory();
+
+        /// <summary>
+        /// The Movimiento.
+        /// </summary>
+        /// <param name="product">The product<see cref="ProductRepositoryDto"/>.</param>
+        /// <returns>The <see cref="bool"/>.</returns>
+        bool Movimiento(ProductRepositoryDto product);
+    }
+}
---
+++ CLL.Domain/Services/ProductDomain.cs
@@ -0,0 +1,53 @@
+﻿// <copyright file="ProductDomain.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CLL.Domain.Services
+{
+    using CLL.Domain.Interfaces;
+    using CLL.Entities.Dto.Repository;
+    using CLL.Infrastructure.Interfaces;
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Defines the <see cref="ProductDomain" />.
+    /// </summary>
+    public class ProductDomain : IProductDomain
+    {
+        /// <summary>
+        /// Defines the _clienteRepository.
+        /// </summary>
+        private readonly Lazy<IProductRepository> _clienteRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProductDomain"/> class.
+        /// </summary>
+        /// <param name="clienteRepository">The clienteRepository<see cref="Lazy{ILoginRepository}"/>.</param>
+        public ProductDomain(Lazy<IProductRepository> clienteRepository)
+        {
+            _clienteRepository = clienteRepository;
+        }
+
+        /// <inheritdoc/>
+        public List<ProductRepositoryDto> Inventory()
+        {
+            return _clienteRepository.Value.ObtenerProductos().Result;
+        }
+
+        /// <inheritdoc/>
+        public bool Movimiento(ProductRepositoryDto product)
+        {
+            if (product.id == 0)
+            {
+                _clienteRepository.Value.Agregar(product);
+            }
+            else
+            {
+                _clienteRepository.Value.Actualizar(product);
+            }
+
+            return true;
+        }
+    }
+}
