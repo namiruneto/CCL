0b954963f0b9dd9035a371121a64715b2ee64dd8
namiruneto <cesfernando1212@gmail.com>
2025-03-06 22:04:06 -0500

    feat: Añadir ApplicationDbContext y configuración inicial
    Se ha implementado la clase `ApplicationDbContext` que hereda de `DbContext`. Incluye un constructor que acepta opciones de configuración y una propiedad `DbSet` para `usuarios`, representando una colección de `LoginRepositoryDto`. También se ha añadido el método `OnModelCreating` para la configuración del modelo.

---
+++ CLL.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿namespace CLL.Infrastructure.Data
+{
+    using CLL.Entities.Dto.Repository;
+    using Microsoft.EntityFrameworkCore;
+
+    /// <summary>
+    /// Defines the <see cref="ApplicationDbContext" />.
+    /// </summary>
+    public class ApplicationDbContext : DbContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApplicationDbContext"/> class.
+        /// </summary>
+        /// <param name="options">The options<see cref="DbContextOptions{ApplicationDbContext}"/>.</param>
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the usuarios.
+        /// </summary>
+        public DbSet<LoginRepositoryDto> usuarios { get; set; }
+
+        /// <inheritdoc/>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
